9f892a7808d63fef5e955001f58cf61224463e46
Joke Heyndels <jo_hannah@outlook.com>
2020-12-27 19:09:18 +0100

    use const in script.js;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 
-setInterval(function() {
-let now = new Date();
-let newYearStart = new Date(now.getFullYear() + 1, 0, 01);
-let timeUntilNewYear = new Date(newYearStart - now);
-months = timeUntilNewYear.getMonth()
-days = timeUntilNewYear.getDate() - 1
-hours = timeUntilNewYear.getHours() - 1
-minutes = timeUntilNewYear.getMinutes()
-seconds = timeUntilNewYear.getSeconds()
+setInterval(function () {
+    const now = new Date();
+    const newYearStart = new Date(now.getFullYear() + 1, 0, 01);
+    const timeUntilNewYear = new Date(newYearStart - now);
+    const months = timeUntilNewYear.getMonth()
+    const days = timeUntilNewYear.getDate() - 1
+    const hours = timeUntilNewYear.getHours() - 1
+    const minutes = timeUntilNewYear.getMinutes()
+    const seconds = timeUntilNewYear.getSeconds()
 
-document.getElementById("months").innerHTML = months;
-document.getElementById("days").innerHTML = days;
-document.getElementById("hours").innerHTML = hours;
-document.getElementById("minutes").innerHTML = minutes;
-document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("months").innerHTML = months;
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
 }, 1000)
